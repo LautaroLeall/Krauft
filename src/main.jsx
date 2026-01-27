2cfe3253291e5447e5aea81bce662814ee21bfdd
LautaroLeall <lautaroleal4@gmail.com>
2026-01-27 18:03:52 -0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/variables.css";
-import "./styles/globals.css";
+import "./index.css";
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
