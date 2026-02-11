817e40c564987e9d849f1bb0abfb874eaee1b6c4
LautaroLeall <lautaroleal4@gmail.com>
2026-02-11 17:33:40 -0300

    add info (dev) in the foooter

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,8 @@
 import { Mail, Instagram } from "lucide-react";
+import {
+    FaGithub,
+    FaLinkedin
+} from 'react-icons/fa'
 import "./Footer.css";
 
 export default function Footer() {
@@ -12,7 +16,7 @@ export default function Footer() {
                 <div className="footer-brand">
                     <span className="footer-logo">krauft<span className="text-lime">.</span></span>
                     <span className="divider">|</span>
-                    <p className="copyright">
+                    <p className="copyright m-0 p-0">
                         © {currentYear} Todos los derechos reservados.
                     </p>
                 </div>
@@ -36,6 +40,30 @@ export default function Footer() {
                 </div>
 
             </div>
+
+            <div className="footer-dev d-flex align-items-center pt-2 gap-3">
+                <div className="redes-dev d-flex gap-3">
+                    <a href="https://github.com/LautaroLeall"
+                        target="_blank"
+                        className='github text-decoration-none'>
+                        <FaGithub className='fs-6' />
+                    </a>
+                    <a href="https://www.linkedin.com/in/lauldp/"
+                        target="_blank"
+                        className='linkedin text-decoration-none'>
+                        <FaLinkedin className='fs-6' />
+                    </a>
+                </div>
+                <div className="text-dev d-flex align-items-center gap-1">
+                    <p className="p-0 m-0 copyright">
+                        Desarrollado por
+                    </p>
+                    <a href="https://wa.me/5493813399463" target="_blank" rel="noopener noreferrer" className="copyright">
+                        Lautaro Leal
+                    </a>
+                </div>
+            </div>
+
         </footer>
     );
 }
